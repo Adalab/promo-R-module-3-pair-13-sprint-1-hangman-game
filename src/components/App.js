ea2dcbc6aa36ad78fd4fbfa26a97d3eb1e817971
marocena26 <mariaelena.arocena@gmail.com>
2022-11-23 10:50:58 +0100

    add const handleButton and paint hangman

--- src/components/App.js
+++ src/components/App.js
@@ -2,11 +2,14 @@ import "../styles/App.scss";
 import { useState } from "react";
 
 function App() {
-  const [numberOfErrors, setNumberOfErrors] = useState(0);
+  const [numberOfErrors, setNumberOfErrors] = useState(0); 
+  
   const handleButton = (ev) => {
     ev.preventDefault();
-    numberOfErrors = numberOfErrors + 1;
-    console.log(handleButton);
+    const increment = numberOfErrors +1;
+    setNumberOfErrors(increment); 
+    //lo que queremos es que cuando se clicke, numberOfErrors, añada un número más. Para ello tenemos que pasarle este numberOfErrors como parámetro a su función  setNumberOfErrors.
+    console.log(numberOfErrors);
   };
 
   return (
@@ -14,6 +17,7 @@ function App() {
       <header>
         <h1 className="header__title">Juego del ahorcado</h1>
       </header>
+      <button onClick={handleButton}> Incrementar </button>
       <main className="main">
         <section>
           <div className="solution">
@@ -39,7 +43,7 @@ function App() {
             />
           </form>
         </section>
-        <section>
+        <section className= {`dummy error- ${numberOfErrors}`} > 
           <span className="error-13 eye"></span>
           <span className="error-12 eye"></span>
           <span className="error-11 line"></span>
@@ -54,7 +58,6 @@ function App() {
           <span className="error-2  line"></span>
           <span className="error-1  line"></span>
         </section>
-        <button onClick={handleButton}> Incrementar </button>
       </main>
     </div>
   );
