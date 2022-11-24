c0dd0972a60d5d4d3ba42d659b1fa634cbc0cf0a
Laura Garcia <lgarciacuello@gmail.com>
2022-11-24 11:32:34 +0100

    resolve errors

--- src/components/App.js
+++ src/components/App.js
@@ -2,31 +2,39 @@ import "../styles/App.scss";
 import { useState } from "react";
 
 function App() {
+  //Variables estados
   const [numberOfErrors, setNumberOfErrors] = useState(0); //estado del contador
-  const [lastLetter, setLastLetter] = useState(''); //estado del input
-  
+  const [lastLetter, setLastLetter] = useState(""); //estado del input
+  const [word, setWord] = useState("katakroker"); //almacenar la palabra de la API
+  const [userLetters, setUsesLeters] = useState([]); //array para almacenar las letras que introduce la jugadora.
+
   //función para incrementar el contador
   const handleButton = (ev) => {
     ev.preventDefault();
-    const increment = numberOfErrors +1;
-    setNumberOfErrors(increment); 
-    console.log(numberOfErrors);
+    const increment = numberOfErrors + 1;
+    setNumberOfErrors(increment);
   };
 
   //función para obtener lo que escribe la usuaria en el input
   const handleInput = (ev) => {
     const lettersAllowed = /[A-Za-z]/; //expresión regular
-    if (lettersAllowed.test(ev.target.value) === true){
-      setLastLetter(ev.target.value); 
-    }; //hacemos una condicional: le decimos que si el valor que introduce la usuaria es igual a los caracteres permitidos, entonces me guarde la letra, sino no.
+    if (lettersAllowed.test(ev.target.value) === true) {
+      setLastLetter(ev.target.value);
+    } //hacemos una condicional: le decimos que si el valor que introduce la usuaria es igual a los caracteres permitidos, entonces me guarde la letra, sino no.
   };
 
   //función para que no se recarge la página al dar al enter.
   const handleSubmit = (ev) => {
     ev.preventDefault();
   };
-
-
+  //funcion para pintar
+  const renderSolutionLetters = () => {
+    const wordLetters = word.split("");
+    // setWord([...wordLetters]);
+    return wordLetters.map((word) => {
+      return <li className="letter"> {word}</li>;
+    });
+  };
 
   return (
     <div className="page">
@@ -38,7 +46,7 @@ function App() {
         <section>
           <div className="solution">
             <h2 className="title">Solución:</h2>
-            <ul className="letters"></ul>
+            <ul className="letters">{renderSolutionLetters()}</ul>
           </div>
           <div className="error">
             <h2 className="title">Letras falladas:</h2>
@@ -61,7 +69,7 @@ function App() {
             />
           </form>
         </section>
-        <section className= {`dummy error- ${numberOfErrors}`} > 
+        <section className={`dummy error-${numberOfErrors}`}>
           <span className="error-13 eye"></span>
           <span className="error-12 eye"></span>
           <span className="error-11 line"></span>
